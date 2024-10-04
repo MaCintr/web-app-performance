c205ecd0b98feb8757f9a1bb25deeb376a4783a0
Marcelo <129231889+MaCintr@users.noreply.github.com>
2024-10-03 21:12:53 -0300

    Add files via upload

---
+++ web-app-repository/UsuarioRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using MySqlConnector;
+using System.Security.Cryptography.X509Certificates;
+using web_app_domain;
+
+namespace web_app_repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly MySqlConnection mySqlConnection;
+
+        public UsuarioRepository()
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            mySqlConnection = new MySqlConnection(connectionString);
+        }
+
+        public async Task<IEnumerable<Usuario>> ListarUsuarios()
+        {
+            await mySqlConnection.OpenAsync();
+            string query = "select Id, Nome, Email from usuarios;";
+            var usuarios = await mySqlConnection.QueryAsync<Usuario>(query);
+            mySqlConnection.Close();
+
+            return usuarios;
+        }
+
+        public async Task SalvarUsuario(Usuario usuario)
+        {
+            await mySqlConnection.OpenAsync();
+            string sql = @"insert into usuarios(nome, email) 
+                            values(@nome, @email);";
+            await mySqlConnection.ExecuteAsync(sql, usuario);
+            mySqlConnection.Close();
+        }
+
+        public async Task AtualizarUsuario(Usuario usuario)
+        {
+
+        await mySqlConnection.OpenAsync();
+
+            string sql = @"update usuarios 
+                            set Nome = @nome, 
+	                            Email = @email
+                            where Id = @id;";
+
+            await mySqlConnection.ExecuteAsync(sql, usuario);
+            await mySqlConnection.CloseAsync();
+        }
+
+        public async Task RemoverUsuario(int id)
+        {
+
+        await mySqlConnection.OpenAsync();
+
+            string sql = @"delete from usuarios where Id = @id;";
+
+            await mySqlConnection.ExecuteAsync(sql, new { id });
+            await mySqlConnection.CloseAsync();
+        }
+    }
+
+    }
+
+    
+
